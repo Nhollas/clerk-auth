7276856617f4adf6507c922a7907fd1ce15ec74f
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-02-08 13:01:16 +0000

    another fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export const Layout = ({ children }) => {
+export const Layout = ({ children }: { children: any }) => {
   return (
     <div className='w-full'>
       <Head>
